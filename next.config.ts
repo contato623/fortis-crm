cf4928fbe2bb32f609d6ea3b4340134684249059
vander <contato@fortisbrasilcorretoraseg.com>
2026-03-28 07:00:03 -0400

    fix vercel config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["127.0.0.1", "192.168.0.5", "localhost"],
+  output: "standalone",
 };
 
 export default nextConfig;
